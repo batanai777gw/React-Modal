35973a98e67d1c68e2f0685e82ad0520c4e1d2a6
Batanai Gwanyanya <batanai178@gmail.com>
2022-09-22 15:13:42 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Modal from "./Components.js/Modal";
+
+function App() {
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className="App">
+      <h1>Hey, click on the button to open the modal.</h1>
+      <button
+        className="openModalBtn"
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        Open
+      </button>
+      {openModal && <Modal closeModal={setOpenModal}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components.js/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Modal.css"
+
+function Modal({ closeModal }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button onClick={() => closeModal(false)}> X </button>
+        </div>
+        <div className="title">
+          <h1>Are You Sure You Want To Continue?</h1>
+        </div>
+        <div className="body">
+          <p>
+            The next page is awesome! You should move forward, you will enjoy
+          </p>
+        </div>
+        <div className="footer">
+          <button onClick={() => closeModal(false)} id="cancelBtn">Cancel</button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
